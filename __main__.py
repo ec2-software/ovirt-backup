84f31c7ff90a08b3d0f6b005b2b678d7696bcadf
Jonathan Duck <duckbrain30@gmail.com>
2018-05-24 09:47:36 -0700

    Allow layering config files with defaults

--- __main__.py
+++ __main__.py
@@ -5,6 +5,7 @@ import backup.backends
 import logging
 import argparse
 import os
+import pprint
 
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
@@ -17,16 +18,26 @@ def str2bool(v):
 parser = argparse.ArgumentParser(description="Backup Ovirt VMs")
 parser.add_argument("--config", dest="config_file", default="config.yml", help="set config file path")
 parser.add_argument("--search", dest="search", default=None, help="search for specific VM name")
-parser.add_argument("--umount", type=str2bool, dest="umount", default=True, help="Unmount the drives and detach the disks")
+parser.add_argument("--umount", type=str2bool, dest="umount", default=True, help="Unmount the drives and detach the disks") 
+parser.add_argument("--show-config", type=str2bool, dest="show_config", default=False, help="Show the configuration options instead of performing a backup") 
 args = parser.parse_args()
 
-#defaults_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "defaults.yml")
+defaults_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "defaults.yml")
 
-#for file in [ defaults_file, "/etc/ovirt-backup/config.yml", args.config_file ]:
+config = {}
+for file in [ defaults_file, "/etc/ovirt-backup/config.yml", args.config_file ]:
+    print("Loading config file {}".format(file))
+    try:
+        with open(file, "r") as c:
+            backup.util.dict_merge(config, yaml.safe_load(c))
+    except IOError:
+        print("Failed to load {}".format(file))
+        pass
 
-
-with open(args.config_file) as c:
-    config = yaml.safe_load(c)
+if args.show_config:
+    pp = pprint.PrettyPrinter(indent=2)
+    pp.pprint(config)
+    exit()
 
 logging.basicConfig(level=logging.DEBUG, filename='/var/log/ovirt-backup.log')
 #logging.getLogger().addHandler(logging.StreamHandler())
--- backup/__init__.py
+++ backup/__init__.py
@@ -1,3 +1,4 @@
 from .backend import Backend
 from .backup import Backup
 from .mount import MountBackend
+from .util import dict_merge
